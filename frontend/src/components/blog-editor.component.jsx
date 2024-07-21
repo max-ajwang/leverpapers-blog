efb152739ec3b16a5e5596fa8541175339dfd48e
max-ajwang <maxajwang@gmail.com>
2024-07-22 00:29:57 +0300

    Blog Editor component

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+
 const BlogEditor = () => {
-  return <h1>Blog editor from component</h1>;
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} />
+        </Link>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2">publish</button>
+          <button className="btn-light py-2">Save draft</button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default BlogEditor;
